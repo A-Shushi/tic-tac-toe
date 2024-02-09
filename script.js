abfd2f1b4d9daf21a929164f9988968a79ac00f4
Andrei <124041084+A-Shushi@users.noreply.github.com>
2024-02-09 18:29:49 +0700

    Game has working buttons

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 const gameArea = document.querySelector(".gameboard-div")
-const textField = document.querySelector("#index")
-const submitButton = document.querySelector("#submit-btn")
 
 let gameboard = (function () {
     let gameboardArray = ["", "", "", "", "", "", "", "", ""]
     let render = function () {
+        let i = 0
         console.log(gameboardArray)
         while (gameArea.firstChild) {
             gameArea.removeChild(gameArea.lastChild)
         }
         gameboardArray.forEach((element) => {
             let newButton = document.createElement("button")
+            newButton.setAttribute("data-index", i)
             newButton.textContent = element
             gameArea.appendChild(newButton)
+            i++
         })
     }
     let arrayUpdate = function (index, sign) {
@@ -45,30 +46,38 @@ let game = (function () {
         }
     }
     let selectPosition = function () {
-        if (gameOn) {
-            let index = textField.value
-            textField.textContent = ""
-            gameboard.arrayUpdate(index, currentPlayer.sign)
-            let currentArray = gameboard.getArray()
-            if (currentArray[0] === currentArray[1] && currentArray[1] === currentArray[2] && currentArray[0] !== "" ||
-                currentArray[3] === currentArray[4] && currentArray[4] === currentArray[5] && currentArray[3] !== "" ||
-                currentArray[6] === currentArray[7] && currentArray[7] === currentArray[8] && currentArray[6] !== "" ||
-                currentArray[0] === currentArray[3] && currentArray[3] === currentArray[6] && currentArray[0] !== "" ||
-                currentArray[1] === currentArray[4] && currentArray[4] === currentArray[7] && currentArray[1] !== "" ||
-                currentArray[2] === currentArray[5] && currentArray[5] === currentArray[8] && currentArray[2] !== "" ||
-                currentArray[0] === currentArray[4] && currentArray[4] === currentArray[8] && currentArray[0] !== "" ||
-                currentArray[2] === currentArray[4] && currentArray[4] === currentArray[6] && currentArray[2] !== "") {
-                gameOn = false
-                console.log("Game Over " + currentPlayer.name + " Won!")
-            } else if (currentArray[0] && currentArray[1] && currentArray[2] &&
-                       currentArray[3] && currentArray[4] && currentArray[5] &&
-                       currentArray[6] && currentArray[7] && currentArray[8] !== "") {
-                gameOn = false
-                console.log("Game Over! It's a Draw!");
-            }
-            playerSwitch()
+        let index = this.getAttribute("data-index")
+        gameboard.arrayUpdate(index, currentPlayer.sign)
+        buttonAssign()
+        checkCondition()
+        playerSwitch()
+    }
+    let checkCondition = function () {
+        let currentArray = gameboard.getArray()
+        if (currentArray[0] === currentArray[1] && currentArray[1] === currentArray[2] && currentArray[0] !== "" ||
+            currentArray[3] === currentArray[4] && currentArray[4] === currentArray[5] && currentArray[3] !== "" ||
+            currentArray[6] === currentArray[7] && currentArray[7] === currentArray[8] && currentArray[6] !== "" ||
+            currentArray[0] === currentArray[3] && currentArray[3] === currentArray[6] && currentArray[0] !== "" ||
+            currentArray[1] === currentArray[4] && currentArray[4] === currentArray[7] && currentArray[1] !== "" ||
+            currentArray[2] === currentArray[5] && currentArray[5] === currentArray[8] && currentArray[2] !== "" ||
+            currentArray[0] === currentArray[4] && currentArray[4] === currentArray[8] && currentArray[0] !== "" ||
+            currentArray[2] === currentArray[4] && currentArray[4] === currentArray[6] && currentArray[2] !== "") {
+            gameOn = false
+            console.log("Game Over " + currentPlayer.name + " Won!")
+        } else if (currentArray[0] && currentArray[1] && currentArray[2] &&
+            currentArray[3] && currentArray[4] && currentArray[5] &&
+            currentArray[6] && currentArray[7] && currentArray[8] !== "") {
+            gameOn = false
+            console.log("Game Over! It's a Draw!");
         }
     }
-    submitButton.addEventListener("click", selectPosition)
-    return {}
-})()
+    let buttonAssign = function() {
+        let buttons = document.querySelectorAll("button")
+        buttons.forEach((button) => {
+            button.addEventListener("click", selectPosition)
+        })
+    }
+    buttonAssign()
+})
+
+game()
