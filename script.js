573cedd63d4383436c28fab9c99fdfc83299f400
Andrei <124041084+A-Shushi@users.noreply.github.com>
2024-02-10 18:49:19 +0700

    Full working game

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const gameArea = document.querySelector(".gameboard-div")
+const resetButton = document.querySelector("#reset")
+const nameButton = document.querySelector("#change-name")
+const playerXText = document.querySelector("#playerX")
+const playerOText = document.querySelector("#playerO")
+const alertText = document.querySelector("#alert")
 
 let gameboard = (function () {
     let gameboardArray = ["", "", "", "", "", "", "", "", ""]
@@ -10,8 +15,14 @@ let gameboard = (function () {
         }
         gameboardArray.forEach((element) => {
             let newButton = document.createElement("button")
+            newButton.className = "game-btn"
             newButton.setAttribute("data-index", i)
-            newButton.textContent = element
+            if (element !== "") {
+                newButton.textContent = element
+                newButton.disabled = true
+            } else {
+                newButton.textContent = element
+            }
             gameArea.appendChild(newButton)
             i++
         })
@@ -23,7 +34,11 @@ let gameboard = (function () {
     let getArray = function () {
         return gameboardArray
     }
-    return {render, arrayUpdate, getArray}
+    let clearArray = function () {
+        gameboardArray = ["", "", "", "", "", "", "", "", ""]
+    }
+
+    return {render, arrayUpdate, getArray, clearArray}
 })()
 
 let createUser = function (name, sign) {
@@ -32,11 +47,15 @@ let createUser = function (name, sign) {
     return {name, sign}
 }
 
-let game = (function () {
+let game = function () {
     let gameOn = true
+    gameboard.clearArray()
+    alertText.textContent = ""
     gameboard.render()
-    let playerX = createUser("Igor", "X")
-    let playerO = createUser("Kirill", "O")
+    let playerX = createUser(prompt("Player 1 Name:"), "X")
+    playerXText.textContent = `${playerX.name} is playing as ${playerX.sign}`
+    let playerO = createUser(prompt("Player 2 Name:"), "O")
+    playerOText.textContent = `${playerO.name} is playing as ${playerO.sign}`
     let currentPlayer = playerX
     let playerSwitch = function () {
         if (currentPlayer === playerX) {
@@ -52,6 +71,14 @@ let game = (function () {
         checkCondition()
         playerSwitch()
     }
+
+
+    let gameOver = function () {
+        let buttons = document.querySelectorAll(".game-btn")
+        buttons.forEach((button) => {
+            button.disabled = true
+        })
+    }
     let checkCondition = function () {
         let currentArray = gameboard.getArray()
         if (currentArray[0] === currentArray[1] && currentArray[1] === currentArray[2] && currentArray[0] !== "" ||
@@ -63,21 +90,39 @@ let game = (function () {
             currentArray[0] === currentArray[4] && currentArray[4] === currentArray[8] && currentArray[0] !== "" ||
             currentArray[2] === currentArray[4] && currentArray[4] === currentArray[6] && currentArray[2] !== "") {
             gameOn = false
-            console.log("Game Over " + currentPlayer.name + " Won!")
+            alertText.textContent = `${currentPlayer.name} has won!`
+            gameOver()
         } else if (currentArray[0] && currentArray[1] && currentArray[2] &&
             currentArray[3] && currentArray[4] && currentArray[5] &&
             currentArray[6] && currentArray[7] && currentArray[8] !== "") {
             gameOn = false
-            console.log("Game Over! It's a Draw!");
+            alertText.textContent = "It's a draw!"
+            gameOver()
         }
     }
+    let resetGame = function () {
+        nameButton.removeEventListener("click", resetGame)
+        game()
+    }
+
+    let clearGame = function () {
+        gameboard.clearArray()
+        alertText.textContent = ""
+        currentPlayer = playerX
+        gameboard.render()
+        buttonAssign()
+    }
     let buttonAssign = function() {
-        let buttons = document.querySelectorAll("button")
+        let buttons = document.querySelectorAll(".game-btn")
         buttons.forEach((button) => {
             button.addEventListener("click", selectPosition)
         })
     }
     buttonAssign()
-})
+
+    nameButton.addEventListener("click", resetGame)
+    resetButton.addEventListener("click", clearGame)
+}
 
 game()
+
